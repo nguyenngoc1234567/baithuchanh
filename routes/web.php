e743e999aac2b18a49e3d01df2d3fcd5b3cf7215
nguyenngoc1234567 <ngoc2503pp@gmail.com>
2023-03-23 15:28:55 +0700

    Validation form đăng ký

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Http\Controllers\PostController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('bai_tap', function () {
+    return view('bai_tap');
+});
+Route::post('bai_tap', function (Illuminate\Http\Request $request) {
+    $productDescription = $request->input('productDescription');
+    $discountPrice = $request->input('discountPrice');
+    $discountPercent =  $request->input('discountPercent');
+    $discountAmount = $discountPrice * $discountPercent * 0.1;
+    return view('baitap', compact(['discountAmount', 'discountPercent', 'productDescription']));
+});
+Route::get('create', [PostController::class,'create']);
+Route::post('store', [PostController::class,'store'])->name('store');
